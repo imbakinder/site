bdde8f7a432eaca78504484ba50a4849b9568d0d
Oleg <grammy1488@gmail.com>
2023-03-24 21:58:37 +0300

    Добавил дз в script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+
+// const phone = 2007;
+// alert(phone);
+
+// const name = 'Brendan Eich';
+// alert(name);
+
+// const a = 10;
+// const b = 2;
+
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+
+// const result = 2 ** 5;
+// console.log(result);
+
+// const a = 9;
+// const b = 2;
+
+// alert(9 % 2);
+
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+
+// let age = Number(prompt('Сколько Вам лет?'));
+
+// alert(age);
+
+// const user = {
+//     name: 'oleg',
+//     age: 27,
+//     isAdmin: true,
+//     'city of residence': true
+// }
+
+// console.log(user['city of residence']);
+
+// const user = {
+//   name: "oleg",
+//   height: 180,
+//   isAdmin: true,
+//   "city of residence": true,
+// };
+
+// delete user["city of residence"];
+
+// console.log(user);
+
+// const user = {
+//     name: "oleg",
+//     height: 180,
+//     isAdmin: true,
+//     "city of residence": true,
+//   };
+
+//   let info = prompt('Какую информацию хотите узнать о пользователе?');
+
+//   console.log(user[info]);
+
+// let name = prompt("Как Вас зовут?");
+
+// alert(`Привет, ${name} !`);
